360fb1aec59303b47bfe54c13666ce007e0a545c
kenedii <55466055+kenedii@users.noreply.github.com>
2025-02-23 00:01:36 -0500

    Using list((*nums1, *nums2))
    list((*nums1, *nums2)) is faster runtime than [(*nums1, *nums2)]

--- Hard/4.Median_of_Two_Sorted_Arrays/solution.py
+++ Hard/4.Median_of_Two_Sorted_Arrays/solution.py
@@ -2,7 +2,7 @@
 from math import floor
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2) -> float:
-        arr = [(*nums1, *nums2)] # Unpacking iterables to a tuple and converting to list is much faster than nums1 + nums2
+        arr = list((*nums1, *nums2)) # Unpacking iterables to a tuple and converting to list is much faster than nums1 + nums2
         arr.sort()                   # However, doing nums1+nums2 beats 97.29% memory
         i = len(arr)
         if i%2 == 0: # If array length is even
