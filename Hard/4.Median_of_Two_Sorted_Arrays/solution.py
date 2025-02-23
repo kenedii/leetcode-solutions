9ef5414e649960c3e7205b8f3d13aa4a20b8f8a3
kenedii <55466055+kenedii@users.noreply.github.com>
2025-02-22 23:08:16 -0500

    Update solution.py

--- Hard/4.Median_of_Two_Sorted_Arrays/solution.py
+++ Hard/4.Median_of_Two_Sorted_Arrays/solution.py
@@ -2,9 +2,9 @@
 from math import floor
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2) -> float:
-        arr = list((*nums1, *nums2)) # Unpacking iterables to a tuple and converting to list is much faster than nums1 + nums2
-        arr.sort()
+        arr = [(*nums1, *nums2)] # Unpacking iterables to a tuple and converting to list is much faster than nums1 + nums2
+        arr.sort()                   # However, doing nums1+nums2 beats 97.29% memory
         i = len(arr)
-        if i%2 == 0:
+        if i%2 == 0: # If array length is even
             return (arr[int(i/2-0.5)] + arr[int(i/2+0.5)])/2
         return arr[int(floor(i/2))]
